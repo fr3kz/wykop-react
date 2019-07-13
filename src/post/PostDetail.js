76a4656052a60743c977cd5f1f31c948b866a267
fr3kz <tektmap1@gmail.com>
2019-07-13 14:28:38 +0200

    post image display

--- src/post/PostDetail.js
+++ src/post/PostDetail.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class PostDetail extends Component {
     render() {
 
-        const {title,header,published,likes,dislikes,body} = this.props.post
+        const {title,header,published,likes,dislikes,body,image} = this.props.post
         console.log(this.props.post)
         return (
             <div>
@@ -13,6 +13,7 @@ export default class PostDetail extends Component {
                 <p>{published}</p>
                 <p> {likes} </p>
                 <p> {dislikes} </p>
+                <p> <img src={`http://wykop-app.herokuapp.com${image}`} alt="blum"/> </p>
                 <hr></hr>
             </div>
         )
